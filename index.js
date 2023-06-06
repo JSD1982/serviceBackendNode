9ec2e3b336494989da172314689659d6c2796539
JSD1982 <cardoso@josesegundo.digital>
2023-06-06 17:06:17 -0300

    vercel file

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require("express");
-const app = express();
-const product = require("./api/product");
+require('dotenv').config()
 
-app.use(express.json({ extended: false }));
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
-app.use("/api/product", product);
+// express app
+const app = express()
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port test ${PORT}`));
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    const PORT = process.env.PORT;
+    app.listen(PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error)
+  })

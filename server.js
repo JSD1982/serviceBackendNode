b0a144d999ab0057b2b2176f9a3d8ac553d23fa6
JSD1982 <cardoso@josesegundo.digital>
2023-06-07 11:40:13 -0300

    vercel file

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
+
 const app = express();
-const port = 3000;
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hey this is my API running 🥳");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
 });
+
+// Export the Express API
+module.exports = app;
